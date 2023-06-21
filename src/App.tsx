260e2a16caa60223da8ac4cb5d4b86185057fb44
msilis <mikssilis@gmail.com>
2023-06-21 22:12:58 +0100

    added react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Header from "./Components/Header/header";
 import CatFact from "./Components/CatFact/catFact";
 import Button from "./Components/Button/button";
 import Footer from "./Components/Footer/footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App(): JSX.Element {
   const [getCatFact, setGetCatFact] = useState<string>("");
@@ -14,6 +16,7 @@ function App(): JSX.Element {
       <CatFact getCatFact={getCatFact} />
       <Button setGetCatFact={setGetCatFact} catState={getCatFact} />
       <Footer />
+      <ToastContainer />
     </div>
   );
 }
--- src/Utilities/getCatFact.tsx
+++ src/Utilities/getCatFact.tsx
@@ -1,5 +1,6 @@
 import { SetCatProps } from "../Interfaces/setCatProps";
 import { CatFactResponse } from "../Interfaces/catFactResponse";
+import { toast } from "react-toastify";
 
 export async function getCatFact(props: SetCatProps): Promise<void> {
   try {
@@ -11,6 +12,16 @@ export async function getCatFact(props: SetCatProps): Promise<void> {
 
     props.setGetCatFact(fact.fact);
   } catch (err) {
+    toast.error("There was an error getting your fact.", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
     console.log(err);
   }
 }
