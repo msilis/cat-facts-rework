9d49b1abd56c7f36d258157168a549aa64850a20
msilis <mikssilis@gmail.com>
2023-06-21 21:59:54 +0100

    type check response

--- src/Utilities/getCatFact.tsx
+++ src/Utilities/getCatFact.tsx
@@ -1,15 +1,15 @@
 import { SetCatProps } from "../Interfaces/setCatProps";
+import { CatFactResponse } from "../Interfaces/catFactResponse";
 
 export async function getCatFact(props: SetCatProps): Promise<void> {
   try {
     const response = await fetch("https://catfact.ninja/fact?max_length=64");
-    const fact = await response.json();
+    const fact: CatFactResponse = await response.json();
     if (!response.ok) {
       throw new Error("Failed to fetch fact.");
     }
 
     props.setGetCatFact(fact.fact);
-    return fact;
   } catch (err) {
     console.log(err);
   }
